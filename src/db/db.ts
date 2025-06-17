43e82464d9f6266f50b9fe8039d18afe5dadddc2
AhmedElkharashy74 <ahmedelkharashy47@gmail.com>
2025-06-17 15:14:57 +0300

    production preprations

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from "@prisma/client"
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
+// Prevent multiple instances during dev/hot reload
+export const db =
+  globalForPrisma.prisma ?? new PrismaClient()
 
-const db = globalThis.prisma ?? prismaClientSingleton()
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db
+}
 
 export default db
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
